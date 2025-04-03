d1802d82c81b40112acc1f1575fd94d7141e9a50
Dylan Kirby <dylan.kirby.365@gmail.com>
2025-04-03 21:12:14 +0200

    Progress...

--- tree-sitter-diff/tsdiff.c
+++ tree-sitter-diff/tsdiff.c
@@ -1,6 +1,7 @@
 #include "meyers_sequence_matcher.h"
 #include "test_loader.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -128,105 +129,111 @@ void print_side_by_side_diff(const char *original,
 	}
 }
 
-TSTree *parse_and_update(const char *old_source,
-                         const char *new_source,
-                         TSParser *parser)
+/**
+ * Parse the old and new source allocating a tree for each.
+ */
+bool parse_and_update(const char *old_source,
+                      const char *new_source,
+                      TSParser *parser,
+                      TSTree **old_tree,
+                      TSTree **new_tree)
 {
-	:
+
+	if (!old_source || !new_source || !parser) {
+		return false;
+	}
+
+	// Old parse tree
+	*old_tree =
+	    ts_parser_parse_string(parser, NULL, old_source, strlen(old_source));
+	if (!*old_tree) {
+		return false;
+	}
+
+	// Textual Diff to update tree
+	MSMDiffResult *result = msm_diff(old_source, new_source);
+	if (!result) { // No updates
+		*new_tree = *old_tree;
+		return true;
+	}
+
+	for (int i = 0; i < result->count; i++) {
+		MSMDiffSegment segment = result->segments[i];
+		if (segment.type == EQUAL) {
+			continue; // Skip equal segments
+		}
+		TSInputEdit edit = {
+		    .start_byte = segment.start_a,
+		    .old_end_byte = segment.end_a,
+		    .new_end_byte = segment.end_b,
+		    .start_point = {0, 0},
+		    .old_end_point = {0, 0},
+		    .new_end_point = {0, 0},
+		};
+		ts_tree_edit(*old_tree, &edit);
+	}
+
+	msm_free_diff_result(result);
+
+	// Parse the new tree incrementally
+	*new_tree = ts_parser_parse_string(parser, *old_tree, new_source,
+	                                   strlen(new_source));
+	if (!*new_tree) {
+		ts_tree_delete(*old_tree);
+		return false;
+	}
+
+	return true;
 }
 
 int main()
 {
-	test_loader();
 
-	char *original_code = "int main() {\n"
-	                      "    int x = 0, y = 1;\n"
-	                      "    if (x < y) {\n"
-	                      "        return x;\n"
-	                      "    } else {\n"
-	                      "        return y;\n"
-	                      "    }\n";
-	int original_length = strlen(original_code);
-
-	char *modified_code = "int main() {\n"
-	                      "    int x = 0, y = 1;\n"
-	                      "    /* An extraordinary comment */\n"
-	                      "    if (x > y) {\n"
-	                      "        return x;\n"
-	                      "    } else {\n"
-	                      "        return y;\n"
-	                      "    }\n";
-	int modified_length = strlen(modified_code);
+	TestSuites suites = load_test_suites("tests");
+	if (suites.suites == NULL || suites.count == 0) {
+		printf("Failed to load suites\n");
+		return 1;
+	}
 
 	// Create a parser
 	TSParser *parser = ts_parser_new();
 	ts_parser_set_language(parser, tree_sitter_c());
 	ts_parser_set_included_ranges(parser, NULL, 0);
 
-	// Parse the original code
-	TSTree *tree =
-	    ts_parser_parse_string(parser, NULL, original_code, original_length);
-	if (!tree) {
-		fprintf(stderr, "Failed to parse the original code\n");
-		return 1;
-	}
+	for (size_t i = 0; i < suites.count; i++) {
+		TestSuite *suite = &suites.suites[i];
+		printf("Test Suite %zu:\n", i + 1);
+		for (size_t j = 0; j < suite->count; j++) {
+			TestCase *test_case = &suite->cases[j];
+			if (strcmp(test_case->language, "c") != 0) continue;
 
-	// Edit the original tree to reflect the changes (< to >)
-	MSMDiffResult *result = msm_diff(original_code, modified_code);
-	if (result) {
-		for (int i = 0; i < result->count; i++) {
-			MSMDiffSegment segment = result->segments[i];
-			if (segment.type == EQUAL) {
-				continue; // Skip equal segments
+			// Print case
+			printf("Testing case: %s\n", test_case->name);
+
+			TSTree *old_tree = NULL, *new_tree = NULL;
+			bool res = parse_and_update(test_case->original_source,
+			                            test_case->modified_source, parser,
+			                            &old_tree, &new_tree);
+			if (!res) {
+				printf("FAILED TO PROCESS TREES\n");
+				continue;
 			}
-			printf("Segment %d: Type %s\t| A[%d:%d], B[%d:%d]\n", i,
-			       segment.type == EQUAL    ? "EQUAL"
-			       : segment.type == INSERT ? "INSERT"
-			       : segment.type == DELETE ? "DELETE"
-			                                : "REPLACE",
-			       segment.start_a, segment.end_a, segment.start_b,
-			       segment.end_b);
 
-			// This only works because the files are UTF-8 encoded
-			TSInputEdit edit = {
-			    .start_byte = segment.start_a,
-			    .old_end_byte = segment.end_a,
-			    .new_end_byte = segment.end_b,
-			    .start_point = {0, 0},
-			    .old_end_point = {0, 0},
-			    .new_end_point = {0, 0},
-			};
-			ts_tree_edit(tree, &edit);
+			// Get the changed ranges
+			unsigned int range_count;
+			TSRange *ranges =
+			    ts_tree_get_changed_ranges(old_tree, new_tree, &range_count);
+
+			print_changed_ranges(ranges, range_count,
+			                     test_case->modified_source);
+
+			// Free resources
+			free(ranges);
+			ts_tree_delete(old_tree);
+			ts_tree_delete(new_tree);
 		}
-		printf("\n");
-
-		// Print side-by-side diff
-		print_side_by_side_diff(original_code, modified_code, result->segments,
-		                        result->count);
-		printf("\n");
-
-		// Free the diff result
-		msm_free_diff_result(result);
 	}
 
-	// Parse the modified code using the old tree
-	TSTree *new_tree =
-	    ts_parser_parse_string(parser, tree, modified_code, modified_length);
-	if (!new_tree) {
-		fprintf(stderr, "Failed to parse the modified code\n");
-		return 1;
-	}
-
-	// Get the changed ranges
-	unsigned int range_count;
-	TSRange *ranges = ts_tree_get_changed_ranges(tree, new_tree, &range_count);
-
-	print_changed_ranges(ranges, range_count, modified_code);
-
-	// Free resources
-	free(ranges);
-	ts_tree_delete(tree);
-	ts_tree_delete(new_tree);
 	ts_parser_delete(parser);
 
 	return 0;
